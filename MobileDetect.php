41ca5620d7f8d0b508d4dc1de5ee84154d51f73b
ruslan <ryslanfree@yandex.ru>
2015-10-06 22:53:29 +0300

    add lib and namespaces

---
+++ MobileDetect.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruslan
+ * Date: 10/6/15
+ * Time: 10:14 PM
+ */
+namespace rlabuta\mobiledetect;
+
+use Yii;
+use yii\base\Component;
+use rlabuta\mobiledect\lib\MobileDetectLibrary;
+
+class MobileDetect extends Component
+{
+    /**
+     * @var lib\MobileDetectLibrary
+     */
+    private $_mobileDetectLibrary;
+
+    /**
+     *
+     */
+    public function init()
+    {
+        $this->_mobileDetectLibrary = new MobileDetectLibrary();
+    }
+
+}
