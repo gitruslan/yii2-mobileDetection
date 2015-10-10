72d79e17059c1b4cecfb87b0015a4db5f6462729
ruslan <ryslanfree@yandex.ru>
2015-10-10 19:30:16 +0300

    add mobile detect method

--- MobileDetect.php
+++ MobileDetect.php
@@ -9,21 +9,53 @@ namespace rlabuta\mobiledetect;
 
 use Yii;
 use yii\base\Component;
+use yii\helpers\Url;
 use rlabuta\mobiledetect\lib\MobileDetectLibrary;
 
 class MobileDetect extends Component
 {
     /**
+     * @var Init redirect to mobile
+     * version to specify address
+     */
+    public $autoRedirectToMobile;
+    /**
      * @var lib\MobileDetectLibrary
+     * Object MobileDetectLibrary has main
+     * methods for manipulation with mob devices
      */
     private $_mobileDetectLibrary;
 
     /**
-     *
+     * @inheritdoc
      */
     public function init()
     {
         $this->_mobileDetectLibrary = new MobileDetectLibrary();
+
+        // Auto-redirect to mobile version
+        if ($this->autoRedirectToMobile)
+        {
+            $this->redirectToMobile();
+        }
+    }
+
+    /**
+     * Redirect to mobile version to
+     * specify url in parameter [$autoRedirectToMobile] or $url
+     */
+    public function redirectToMobile($url = '')
+    {
+        if ((URL::base(true) === $url)
+            || (URL::base(true) === $this->autoRedirectToMobile))
+        {
+           return;
+        }
+        // Detect mobile device
+        if ($this->_mobileDetectLibrary->isMobile()) {
+            $url = (!empty($url)) ? $url : $this->autoRedirectToMobile;
+            \Yii::$app->response->redirect($url.\Yii::$app->request->url);
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # yii2-mobileDetection
-mobile detection for yii2 advanced version 
+mobile detection for yii2 advanced version
+
+#Initialisation
+
+Add code below In you config file in section components
+
+'mobileDetection' =>[
+    'class' => 'rlabuta\mobiledetect\MobileDetect',
+    'autoRedirectToMobile' => 'http://m.site',
+],
